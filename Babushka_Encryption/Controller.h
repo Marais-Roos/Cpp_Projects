9dd73072ed53ae66d0e53cf871281978f1100096
MRoos99 <maraisroos@outlook.com>
2023-03-20 13:30:31 +0200

    Edit Controller.h - make expand and reduce private

--- Babushka_Encryption/Controller.h
+++ Babushka_Encryption/Controller.h
@@ -22,13 +22,13 @@ class Controller
     private:
         Babushka** babushkaArr;
         unsigned int numBabushkas;
+        ReturnStruct expandArray(unsigned char* array, unsigned int currentSize, const unsigned char* id, int idSize);
+        ReturnStruct reduceArray(unsigned char* array, unsigned int currentSize, const unsigned char* expectedId, int idSize);
     public:
         Controller(std::string filePath);
         ~Controller();
         ReturnStruct decrypt(const unsigned char* array, unsigned int size);
         ReturnStruct encrypt(const unsigned char* array, unsigned int size);
-        ReturnStruct expandArray(unsigned char* array, unsigned int currentSize, const unsigned char* id, int idSize);
-        ReturnStruct reduceArray(unsigned char* array, unsigned int currentSize, const unsigned char* expectedId, int idSize);
         void printArray(unsigned char* array, unsigned int size);
 };
 
